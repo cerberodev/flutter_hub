ddcbacfe2109bec2d5a9ed77857be527c1f83e29
Pierre Guillen <hi@cerbero.dev>
2020-10-22 12:37:22 -0500

    FInish UI

--- lib/ui/user_detail.dart
+++ lib/ui/user_detail.dart
@@ -8,56 +8,85 @@ class UserDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('User'),
       ),
-      body: Center(
-        child: Card(
-          elevation: 20,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image: 'https://avatars0.githubusercontent.com/u/4?v=4',
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25),
-                child: Text(
-                  'Number ID',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 20),
+            child: Card(
+              elevation: 20,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: 'https://avatars0.githubusercontent.com/u/4?v=4',
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25),
-                child: Text(
-                  '@User',
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Text(
+                      'Number ID',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Text(
+                      '@User',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Text(
+                      'Laboris sint fugiat sunt sit fugiat excepteur excepteur deserunt ut ipsum. Exercitation amet sint nulla fugiat adipisicing dolore non consectetur pariatur laborum nisi anim amet cillum. Sint non voluptate minim.',
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Center(
+                    child: OutlineButton(
+                      onPressed: () {
+                        print('Click');
+                      },
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.person),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('View Profile'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25),
-                child: Text(
-                  'Laboris sint fugiat sunt sit fugiat excepteur excepteur deserunt ut ipsum. Exercitation amet sint nulla fugiat adipisicing dolore non consectetur pariatur laborum nisi anim amet cillum. Sint non voluptate minim.',
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
